d8f03678f739d36abafead7ae9935f459ec2e600
joletteolivas <160787502+joletteolivas@users.noreply.github.com>
2024-10-07 21:34:32 -0600

    Add files via upload

---
+++ Tareaprogramación/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Vehiculo
+    {
+        public string Modelo { get; set; }
+        public double Precio { get; set; }
+
+        public Vehiculo(string modelo, double precio)
+        {
+            Modelo = modelo;
+            Precio = precio;
+        }
+        public class DescuentoVehi
+        {
+            public static double DescuentoVehiAp(double porcentaje, Vehiculo vehiculo)
+            {
+                return vehiculo.Precio - (vehiculo.Precio * 5 / 100);
+            }
+        }
+    }
+    public static class VehiculoExtension
+    {
+        public static void MostrarIn(this Vehiculo vehiculo) 
+        {
+            Console.WriteLine($"Modelo de vehículo:{vehiculo.Modelo}, Su precio:{vehiculo.Precio:C}");
+        }
+    }
+
+    public static class Reserva
+    {
+        public static double TotalCosto(int dias, double DescuentoVehiAp)
+        {
+            return DescuentoVehiAp + ( 3000 * dias);
+        }
+    }
+}
---
+++ Tareaprogramación/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo vehiculo = new Vehiculo("Chevrolet Camaro", 1114960);
+
+            Console.WriteLine("Información del vehículo:");
+            vehiculo.MostrarIn();
+
+            double precioYyDescuento = Vehiculo.DescuentoVehi.DescuentoVehiAp(5, vehiculo);
+
+            vehiculo.Precio = precioYyDescuento;
+
+            Console.WriteLine("\nInformación del vehículo más descuento:");
+            vehiculo.MostrarIn();
+
+            int diasReserva = 2;
+            double TotalCostos = Reserva.TotalCosto (diasReserva, precioYyDescuento);
+
+            Console.WriteLine($"\nCosto total más reserva {diasReserva} de dos días:{TotalCostos:C}");
+        }
+    }
+}
